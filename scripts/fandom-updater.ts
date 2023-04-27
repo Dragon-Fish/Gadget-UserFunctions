0f9a82fd2d10f84dd5a05687b90897e2baa44105
Dragon-Fish <dragon-fish@qq.com>
2023-04-28 01:15:28 +0800

    fix: login error for Fandom wiki

--- scripts/fandom-updater.ts
+++ scripts/fandom-updater.ts
@@ -7,23 +7,40 @@ const WIKI_ENDPOINT = 'https://dev.fandom.com/api.php'
 const WIKI_TITLE = 'MediaWiki:UserFunctions.js'
 const FILE_NAME = resolve(__dirname, '../lib/index.js')
 
-const app = new MwApi(WIKI_ENDPOINT)
-app
-  .login(process.env.MW_USERNAME as string, process.env.MW_PASSWORD as string)
-  .then(async () => {
-    const gitLog = execSync('git log -n 1 --pretty="format:%h %s by @%an"')
-      .toString()
-      .trim()
-    const summary = `[GitHub] ${gitLog}`
+;(async () => {
+  const app = new MwApi(WIKI_ENDPOINT)
+  // @FIXME this is not work for Fandom
+  await app
+    .login(process.env.MW_USERNAME as string, process.env.MW_PASSWORD as string)
+    .then((data) => {
+      if (data.status !== 'PASS') {
+        return Promise.reject(data)
+      }
+      console.info(`[Login ${data.status}]`, data)
+    })
+    .catch((e) => {
+      console.error('[Login ERROR]', e)
+      throw e
+    })
 
-    const text = (await readFile(FILE_NAME)).toString()
+  const gitLog = execSync('git log -n 1 --pretty="format:%h %s by @%an"')
+    .toString()
+    .trim()
+  const summary = `[GitHub] ${gitLog}`
+  const text = (await readFile(FILE_NAME)).toString()
 
-    const result = await app.postWithToken('csrf', {
+  await app
+    .postWithToken('csrf', {
       action: 'edit',
       title: WIKI_TITLE,
       text,
       summary,
     })
-
-    console.log('All done', result.data)
-  })
+    .then(({ data }) => {
+      console.log('[Submit DONE]', data)
+    })
+    .catch((e) => {
+      console.error('[Submit ERROR]', e)
+      throw e
+    })
+})()
